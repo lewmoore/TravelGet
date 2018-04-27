45deec782cea074f4dac99f9e8ad8ed4535cbb78
Daniel Miller <danielmiller5791@gmail.com>
2018-04-27 11:50:24 +0100

    api dataobtained, need to setup mongodb and rest of application

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-const fastify = require('fastify')()
+const fastify = require('fastify')();
 
-fastify.get('/', function(request, reply){
-  reply.send({ hello: 'world '})
-})
-
-fastify.listen(3000, '127.0.0.1', function (err) {
-  if (err) throw err
-  console.log(`server listening on ${fastify.server.address().port}`)
-})
+var appId = 'c379f1c3';
+var appKey = '73ea15c39e18152d049f656c8d7d86f7';
+var tfl = require('tfl.api')(appId, appKey);
+tfl.accidentstats(2015).then(r => console.log(r.body));
