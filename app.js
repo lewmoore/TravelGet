b733a4c8fb231e1e9ec5793c33b0fd9370386fbc
Irbe Krumina <irbekrm@gmail.com>
2018-04-27 14:40:49 +0100

    mongo connection installed

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-//const travel = require('./models/travel_data.js');
+const travel = require('./models/travel_data.js');
 mongoose.connect('mongodb://localhost:27017/test_db');
 
 var appId = 'c379f1c3';
 var appKey = '73ea15c39e18152d049f656c8d7d86f7';
-//var tfl = require('tfl.api')(appId, appKey);
+var tfl = require('tfl.api')(appId, appKey);
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
-//tfl.accidentstats(2015).then(r => { console.log(r.body[0]) });
+tfl.accidentstats(2015).then(r => { console.log(r.body[0]), travel.create({id: 23, location: 'Riga'})});
 
 
 //tfl.accidentstats(2015).then(r => console.log(r.body[0]));
