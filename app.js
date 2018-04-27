bce7114374227c92aeabfa8fcd0359888dde1936
Irbe Krumina <irbekrm@gmail.com>
2018-04-27 15:34:28 +0100

    app.js

--- app.js
+++ app.js
@@ -2,21 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const travel = require('./models/travel_data.js');
-mongoose.connect('mongodb://localhost:27017/test_db');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db('test_db');
+  const collection = db.collection('documents');
+  
 var appId = 'c379f1c3';
 var appKey = '73ea15c39e18152d049f656c8d7d86f7';
 var tfl = require('tfl.api')(appId, appKey);
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
-
-tfl.accidentstats(2015).then(r => { console.log(r.body[0]), travel.create({id: 23, location: 'Riga'})});
-
-
-//tfl.accidentstats(2015).then(r => console.log(r.body[0]));
-
-app.listen(9000, () => console.log('App listening on port 9000!'))
+  
+   tfl.accidentstats(2015).then(r => {let data = r.body; 
+     for(let i in r.body) { delete r.body[i].$type; collection.insertOne(r.body[i])}});
+});
