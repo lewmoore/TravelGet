a7eede07aa27ecb9c1c25a2d463cd4c12a3a0a8a
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-27 11:56:33 +0100

    Add initial fastify route to index

--- app.js
+++ app.js
@@ -3,4 +3,15 @@ const fastify = require('fastify')();
 var appId = 'c379f1c3';
 var appKey = '73ea15c39e18152d049f656c8d7d86f7';
 var tfl = require('tfl.api')(appId, appKey);
+
+fastify.get('/', function(request, reply) {
+  reply.send('Hello')
+})
+
+
 tfl.accidentstats(2015).then(r => console.log(r.body));
+
+fastify.listen(3000, '127.0.0.1', function (err) {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+})
