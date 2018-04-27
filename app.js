5be04bd72ce76f600380b15656736ee6f6b0de77
Irbe Krumina <irbekrm@gmail.com>
2018-04-27 14:26:45 +0100

    .

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const fastify = require('fastify')();
-
-fastify.register(require('fastify-mongoose'), {
-  uri: 'mongodb://localhost/test_db'
-}, err => {
-  if (err) throw err
-})
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+//const travel = require('./models/travel_data.js');
+mongoose.connect('mongodb://localhost:27017/test_db');
 
 var appId = 'c379f1c3';
 var appKey = '73ea15c39e18152d049f656c8d7d86f7';
-var tfl = require('tfl.api')(appId, appKey);
+//var tfl = require('tfl.api')(appId, appKey);
 
-fastify.get('/', function(request, reply) {
-  reply.send('Hello');
-});
 
-tfl.accidentstats(2015).then(r => console.log(r.body[0]));
-/*
-fastify.listen(3000, '127.0.0.1', function (err) {
-  if (err) throw err;
-  console.log(`server listening on ${fastify.server.address().port}`);
-});
-*/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+//tfl.accidentstats(2015).then(r => { console.log(r.body[0]) });
+
+
+//tfl.accidentstats(2015).then(r => console.log(r.body[0]));
+
+app.listen(9000, () => console.log('App listening on port 9000!'))
